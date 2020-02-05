6d9d28aa8037fdbf22678ab0f93fbf260c97a975
cedouiri <cedouiri@gmail.com>
2020-02-05 19:40:27 +0000

    fix betty

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef SORT_HEADER
 #define SORT_HEADER
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void swap(int *arr, int idx1, int idx2);
@@ -22,7 +21,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 listint_t *create_listint(const int *array, size_t size);
-void swap_list(listint_t *prev_n, listint_t *temp, listint_t *next_n, listint_t **h);
+void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
